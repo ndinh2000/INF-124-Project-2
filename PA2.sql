ab8c3aa19f37d37765693874cc9217979bf52a9b
minichomp4 <turkkanyasemin@gmail.com>
2021-04-29 17:06:59 -0700

    Added mySQL

---
+++ PA2.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS `petstore`;
+CREATE DATABASE IF NOT EXISTS `petstore`;
+# USE `petstore`;
+
+SET FOREIGN_KEY_CHECKS=0;	-- ???
+
+--
+-- Table structure for table `Pet`
+--
+
+CREATE TABLE `Pet` (
+                       `pet_id` varchar(10) NOT NULL,
+                       `name` varchar(255) NOT NULL,
+                       `age` int NOT NULL,
+                       `gender` enum('male', 'female'),
+                       `price` float NOT NULL,
+                       `message` varchar(1024) NOT NULL,
+                       `profile_picture` varchar(2048) NOT NULL,
+                       PRIMARY KEY (`pet_id`)
+);
+
+INSERT INTO `Pet` VALUES ('D101', 'Lucky', '3', 'male', '2000', 'I am a beautiful boy.' , '/../pics/1.png');
+
+--
+-- Table structure for table `Order`
+--
+
+CREATE TABLE `Order` (
+                         `order_id` int NOT NULL AUTO_INCREMENT,
+                         `pet_id` varchar(10) NOT NULL,
+                         `price` float NOT NULL,
+                         `name_first` varchar(50) NOT NULL,
+                         `name_last` varchar(50) NOT NULL,
+                         `email` varchar(100) NOT NULL,
+                         `address_country` varchar(30) NOT NULL,
+                         `address_zipcode` varchar(30) DEFAULT NULL,
+                         `address_state` varchar(30) DEFAULT NULL,
+                         `address_city` varchar(30) NOT NULL,
+                         `address_street` varchar(30) NOT NULL,
+                         `order_time` datetime NOT NULL,
+                         `card_number` varchar(30) NOT NULL,
+                         `expiration_date` datetime NOT NULL,
+                         shipping_method enum('ground', 'first_overnight', 'two_days'),
+                         PRIMARY KEY (`order_id`),
+                         CONSTRAINT `order_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `Pet` (`pet_id`) ON DELETE NO ACTION
+);
--- src/main/java/ShowListServlet.java
+++ src/main/java/ShowListServlet.java
@@ -6,6 +6,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,60 +29,87 @@ public class ShowListServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ShowListServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>The list of emails:</h1>");
-            out.println("</body>");
-            out.println("</html>");
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet ShowListServlet</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>The list of emails:</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+//    }
+
+
+//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+//    /**
+//     * Handles the HTTP <code>GET</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Handles the HTTP <code>POST</code> method.
+//     *
+//     * @param request servlet request
+//     * @param response servlet response
+//     * @throws ServletException if a servlet-specific error occurs
+//     * @throws IOException if an I/O error occurs
+//     */
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        processRequest(request, response);
+//    }
+//
+//    /**
+//     * Returns a short description of the servlet.
+//     *
+//     * @return a String containing servlet description
+//     */
+//    @Override
+//    public String getServletInfo() {
+//        return "Short description";
+//    }// </editor-fold>
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/"
+                    + "as2", "root", "root");
+            Statement stmt = con.createStatement();
+            String sql = "SELECT name, profile_picture FROM pet";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            PrintWriter writer = resp.getWriter();
+            writer.println("<Html> <body>");
+            String imgPath = "hi";
+            while (rs.next()) {
+                writer.println(rs.getString("name"));
+                imgPath = rs.getString("profile_picture");
+//                writer.println(rs.getString("profile_picture"));
+                writer.println("</br>");
+            }
+            writer.println("<img src= ." + imgPath +"/>");
+            writer.println("</body> </Html> ");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
