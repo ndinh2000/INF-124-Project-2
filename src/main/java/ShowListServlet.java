7966db1f66dd880eb06027be6b40f1fcb0abff4b
minichomp4 <turkkanyasemin@gmail.com>
2021-04-30 00:15:33 -0700

    Dog and Cat data added to SQL file. Animal pictures added to new file. Database name changed to petstore.

--- src/main/java/ShowListServlet.java
+++ src/main/java/ShowListServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,99 +8,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ndinh
- */
 @WebServlet(name = "ShowList", value="/ShowList")
 public class ShowListServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet ShowListServlet</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>The list of emails:</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
-//    }
-
-
-//    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-//    /**
-//     * Handles the HTTP <code>GET</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
-//    /**
-//     * Handles the HTTP <code>POST</code> method.
-//     *
-//     * @param request servlet request
-//     * @param response servlet response
-//     * @throws ServletException if a servlet-specific error occurs
-//     * @throws IOException if an I/O error occurs
-//     */
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        processRequest(request, response);
-//    }
-//
-//    /**
-//     * Returns a short description of the servlet.
-//     *
-//     * @return a String containing servlet description
-//     */
-//    @Override
-//    public String getServletInfo() {
-//        return "Short description";
-//    }// </editor-fold>
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/"
-                    + "as2", "root", "root");
+                    + "petstore", "root", "root");
             Statement stmt = con.createStatement();
-            String sql = "SELECT name, profile_picture FROM pet";
+            String sql = "SELECT name, profile_picture FROM petstore.pet";
             ResultSet rs = stmt.executeQuery(sql);
 
             PrintWriter writer = resp.getWriter();
             writer.println("<Html> <body>");
-            String imgPath = "hi";
+            String imgPath = "";
             while (rs.next()) {
                 writer.println(rs.getString("name"));
                 imgPath = rs.getString("profile_picture");
-//                writer.println(rs.getString("profile_picture"));
                 writer.println("</br>");
             }
-            writer.println("<img src= ." + imgPath +"/>");
+//            writer.println("<img src=" + imgPath +"/>");
+//            writer.println("<img src=\"../../../../pics/1.png\">");
+//            writer.println("<img src=\"../../../pics/1.png\">");
+//            writer.println("<img src=\"../../pics/1.png\">");
+//            writer.println("<img src=\"../pics/1.png\">");
+//            writer.println("<img src=\"pics/1.png\">");
+
             writer.println("</body> </Html> ");
 
         } catch (ClassNotFoundException | SQLException e) {
