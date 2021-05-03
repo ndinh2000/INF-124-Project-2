a1121a7505fe7a83cfceccd1b5b099cca38f0e24
minichomp4 <turkkanyasemin@gmail.com>
2021-05-02 17:24:20 -0700

    Saved Before changes

--- src/main/java/HomeServlet.java
+++ src/main/java/HomeServlet.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeServlet extends HttpServlet {
 
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
+
+            resp.setContentType("text/html;charset=UTF-8");
+            String url1 = "/Last5";
+            String url2 = "/Products";
+
+            RequestDispatcher rd = req.getRequestDispatcher(url1);
+            rd.include(req, resp);
+            rd = req.getRequestDispatcher(url2);
+            rd.include(req, resp);
+
+
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/"
                     + "petstore", "root", "root");
@@ -23,16 +35,14 @@ public class HomeServlet extends HttpServlet {
             ResultSet rs = stmt.executeQuery(sql);
 
 
-
-
             PrintWriter writer = resp.getWriter();
             writer.println("<!DOCTYPE html><html lang=\"en\">\n" +
                     "<head>\n" +
                     "    <meta charset=\"UTF-8\">\n" +
                     "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
                     "    <title>The Pet Shop | Home</title>\n");
-            writer.println("<link rel='stylesheet' type='text/css' href='" + req.getContextPath() +  "/myStyle.css' />\n");
-            writer.println("</head>"+
+            writer.println("<link rel='stylesheet' type='text/css' href='" + req.getContextPath() + "/myStyle.css' />\n");
+            writer.println("</head>" +
                     "<body>\n" +
                     "    <div id=\"header\"><h1><img src=\"images/shopLogo.png\"></h1></div>\n" +
                     "    <div id = \"top-nav-bar\">\n" +
@@ -46,31 +56,52 @@ public class HomeServlet extends HttpServlet {
                     "\n");
 
 
+
             writer.println("<h3 style=\"text-align: left;font-size: 1.5em;margin-left: 34px;margin-bottom: 0;\">Our featured pets:</h3>\n" +
                     "        <!-- <div class=\"featuredPets\"> -->\n" +
                     "            <div class=\"row\" style=\"padding-top: 0;\">");
             writer.println("<Html> <body>");
-            while (rs.next()) {
-//                writer.println(rs.getString("name"));
-                writer.println("<div class=\"col-3 col-s-5 featuredPets\">\n" +
-                        "                    <a href=\"../CatPgs/cat1.html\" style=\"text-decoration: none\">\n" +
-                        "                        <div style=\"height: 275px;\">\n" +
-                        "                            <img src=\"images/CatImages/"+ rs.getString("profile_picture")+"\">\n" +
-                        "                        </div>\n" +
-                        "                        <h3> "+ rs.getString("name") +"- $"+ rs.getString("price") +"</h3>\n" +
-                        "                    </a>\n" +
-                        "                    <p>  "+ rs.getString("message") +"</p>\n" +
-                        "                    <hr class=\"solid\">\n" +
-                        "                </div>");
-            }
-            writer.println("</div>\n" +
-                    "\n" +
-                    "    </div>\n" +
-                    "</body>\n" +
+//            RequestDispatcher rd = req.getRequestDispatcher(url1);
+//            rd.include(req, resp);
+//            rd = req.getRequestDispatcher(url2);
+//            rd.include(req, resp);
+//            while (rs.next()) {
+////                writer.println(rs.getString("name"));
+//                writer.println("<div class=\"col-3 col-s-5 featuredPets\">\n" +
+//                        "                    <a href=\"../CatPgs/cat1.html\" style=\"text-decoration: none\">\n" +
+//                        "                        <div style=\"height: 275px;\">\n" +
+//                        "                            <img src=\"images/CatImages/" + rs.getString("profile_picture") + "\">\n" +
+//                        "                        </div>\n" +
+//                        "                        <h3> " + rs.getString("name") + "- $" + rs.getString("price") + "</h3>\n" +
+//                        "                    </a>\n" +
+//                        "                    <p>  " + rs.getString("message") + "</p>\n" +
+//                        "                    <hr class=\"solid\">\n" +
+//                        "                </div>");
+//            }
+            writer.println("</div>\n" +"\n" +" </div>\n");
+            writer.println("</body>\n" +
                     "</html>");
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
     }
 }
--- src/main/java/Last5.java
+++ src/main/java/Last5.java
@@ -16,21 +16,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author ndinh
- */
+
 public class Last5 extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -39,7 +27,7 @@ public class Last5 extends HttpServlet {
             Connection con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/"
                     + "petstore", "root", "root");
             Statement stmt = con.createStatement();
-            String sql = "SELECT p.name, p.profile_picture" +
+            String sql = "SELECT p.name, p.profile_picture, p.price, p.message" +
                         " FROM pet p, orders o" +
                         " WHERE p.pet_id = o.pet_id" +
                         " AND o.user_id = 1" +
@@ -47,6 +35,7 @@ public class Last5 extends HttpServlet {
             ResultSet rs = stmt.executeQuery(sql);
 
             PrintWriter writer = response.getWriter();
+
             writer.println("<Html> <body>");
             writer.println("<h2>Latest Purchases:</h2>");
             String imgPath = "";
@@ -61,59 +50,11 @@ public class Last5 extends HttpServlet {
                 writer.println("</div>");
                 writer.println("</br>");
             }
-//            writer.println("<img src='./images/1.png'>");
-//            writer.println("<img src='./images/cat1.jpg' >");
-//            writer.println("<img src='./images/CatImages/cat1.jfif' >");
-//            writer.println("<img src=\"../../../../pics/1.png\">");
-//            writer.println("<img src=\"../../../pics/1.png\">");
-//            writer.println("<img src=\"../../pics/1.png\">");
-//            writer.println("<img src=\"../pics/1.png\">");
-//            writer.println("<img src=\"pics/1.png\">");
-
             writer.println("</body> </Html> ");
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
